2a3771f5d27c628538048c5d2ee5333759b8911a
Bairon29 <baironvasquez29@gmail.com>
2017-01-07 15:02:25 -0500

    rename slide to content and slide logic in process

---
+++ tutorial/components/Slide.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+function Slide(props){
+
+return(
+<View>
+<Text>
+props
+</Text>
+</View>
+
+  )
+
+}
+
+export default Slide;
--- tutorial/index.ios.js
+++ tutorial/index.ios.js
@@ -11,27 +11,42 @@ import {
   Text,
   View
 } from 'react-native';
-
+import Slide from './components/Slide';
+import Content from './ios/content';
 export default class tutorial extends Component {
 
   constructor(){
     super();
+    this.State={
+      oldSlide: {
+        text: "",
+        image: ""
+      },
+      currentSlide: {
+        text: "",
+        image: ""
+      },
+      nextSlide: {
+        text: "",
+        image: ""
+      }
+    }
 
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState(old, current, next){
+    setState({
+      oldSlide: old,
+      currentSlide: current,
+      nextSlide: next
+    })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Slide updateState={this.updateState} slide={this.state}/>
       </View>
     );
   }
