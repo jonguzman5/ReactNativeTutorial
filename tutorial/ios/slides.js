9f7f781c22410b8abe492d62d1bc98fa8372a0b7
Kiba Gateaux <kibagateaux@gmail.com>
2017-01-07 14:12:15 -0500

    adds slides file

---
+++ tutorial/ios/slides.js
@@ -0,0 +1,17 @@
+export default = {
+  1:{
+    id:1,
+    text:"Enjoy the gift of knowledge with this React Native intro tutorial",
+    img:""
+  },
+  2:{
+    id:2
+    text:`Native is a framework for building mobile apps ...  in JavaScript!!!
+          These are not mere mini web applications but a stand-alone app,
+          running directly off your phone  `
+    img:""
+  },
+
+
+
+}
