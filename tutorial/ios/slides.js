b72a99d77016383841eff9d08147d376c63a381c
Kiba Gateaux <kibagateaux@gmail.com>
2017-01-08 17:44:46 -0500

    adss slide content

--- tutorial/ios/slides.js
+++ tutorial/ios/slides.js
@@ -1,54 +1,52 @@
 export default = [
   {
-    id:1,
     text:"Enjoy the gift of knowledge with this React Native intro tutorial",
     img:""
   },
   {
-    id:2
+    text:`React: a lot to do a little - Facebook`
+    img:``
+  },
+  {
     text:`Native is a framework for building mobile apps ... in JavaScript!!!
           /n These are not mere mobile web applications but a stand-alone app,
           running directly off your phone.`
     img:""
   },
   {
-    id:3
     text:`What is the difference between ReactJS and React-Native?
-          /n ReactJS is a library, Native is a translator for your
-          personal library (app) into common languages (`
+          /n ReactJS is a library, Native is a translator for books (components)`
     img:""
   },
   {
-    id:4
+    text:`State: the fabulous book that keeps on re-writing.`
+    img:``
+  },
+  {
     text:`REACT NATIVE IS GREAT /n It comes with build in functionality and components
          for text, maps, API calls, and as of yet undiscovered features.
           /n You can even import legacy components from Java and C`
     img:""
   },
   {
-    id:5
     text:`BUT WAIT THERES MORE`
     img:""
   },
   {
-    id:6
     text:`ReactJS and React Native code are 85% compatible across platforms.
           /n What does that mean?`
     img:""
   },
   {
-    id:7
     text: `It means if you make an iOS app, your troll of a summer intern can
           have it running faster than you can say slave labor.`
     img:""
   },
   {
-    id:5
     text:``
     img:""
   },
   {
-    id:8
     text:``
     img:""
   }
